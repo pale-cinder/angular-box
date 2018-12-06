fce492a6b4619f6086cebc02c7f0fc122fc2c762
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-06 15:36:08 -0800

    editing the app input field

--- angular-base/src/app/app.component.ts
+++ angular-base/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first angular-base';
+  // title = 'first angular-base';
+  name = 'pale-cinder';
 }
