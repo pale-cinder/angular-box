71158ffc87774eb0b3323ee311841fe199a05919
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-07 14:21:33 -0800

    addinng content to the component

--- angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+  ];
 
   constructor() { }
 
