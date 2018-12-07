fd1e22c5dc9ffdca50e2622d956d07c064685207
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-06 19:45:57 -0800

    css class with cgClass

--- angular-base/src/app/server/server.component.ts
+++ angular-base/src/app/server/server.component.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 @Component ({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online {
+        color: white;
+    }`]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
 
     constructor() {
-        this.serverStatus = Math.random() > 0,5 ? 'online' : 'offline';
+        //this.serverStatus = Math.random() > 0,5 ? 'online' : 'offline';
     }
 
     getServerStatus() {
