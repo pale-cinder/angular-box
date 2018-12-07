cc69c9a4750bd518d1575c25b1d8dbf224d4c7c8
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-06 19:59:34 -0800

    ngFor

--- angular-base/src/app/app.component.ts
+++ angular-base/src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { log } from 'util';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: blue;
-  }
-  `]
+  styleUrls: [`./app.component.css`]
+//   styles: [`
+//   h3 {
+//     color: blue;
+//   }
+//   `]
 })
 export class AppComponent {
   // title = 'first angular-base';
   // name = 'pale-cinder';
-}
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+  }
+ }
+
