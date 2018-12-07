82b0824d25c59c30847d1e66edceff48e92dca7f
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-06 21:34:50 -0800

    header

--- angular-box/src/app/app.module.ts
+++ angular-box/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
