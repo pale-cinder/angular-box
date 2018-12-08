32a2053f7733fa4206bfbf44be8cacc19afa791a
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-07 19:13:30 -0800

    list created

--- angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,9 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
     new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
-
+  
   constructor() { }
 
   ngOnInit() {
