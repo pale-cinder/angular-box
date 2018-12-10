7020d5a276caca278ca0b10e367f0a4283cc5302
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-09 18:02:01 -0800

    faq component

--- compare-angular/src/app/faq/faq.component.ts
+++ compare-angular/src/app/faq/faq.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-faq',
@@ -6,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.sass']
 })
 export class FaqComponent implements OnInit {
+  
+  faqs: Array<any>;
+  // private results: Observable<faqs[]>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.http.get('http://jsonplaceholder.typicode.com/posts')
+    // .map(response => response.json())
+    .subscribe (res => this.faqs = res);
+  }
+
+
 
   ngOnInit() {
   }
