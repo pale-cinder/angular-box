7633764ee3d55f504a8041030b2130ce5349ba66
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-18 15:34:08 -0800

    passing img update

--- angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -11,7 +11,7 @@ export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-    new Recipe('Test Recipe', 'This is some test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('Test Recipe', 'This is some test for the test', 'https://www.jimmypsbutchershop.com/image/cache/catalog/Double%20rib%20chop_1024x683-900x800.jpg')
   ];
   
   constructor() { }
