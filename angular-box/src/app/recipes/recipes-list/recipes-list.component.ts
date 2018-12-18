abd18aac63baedba63f342a29f485b54dadf97f8
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-18 15:31:45 -0800

    passing data

--- angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-box/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -11,7 +11,7 @@ export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-    new Recipe('Test Recipe', 'This is test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('Test Recipe', 'This is some test for the test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
   
   constructor() { }
