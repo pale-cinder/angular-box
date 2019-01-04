4e51a98449df5e18cf605113cd9003589a50a78a
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-01-03 17:21:37 -0800

    added routerLink

--- angular-box/src/app/header/header.component.ts
+++ angular-box/src/app/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { headersToString } from 'selenium-webdriver/http';
+// import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>();
-        onSelect(feature: string) {
-            this.featureSelected.emit(feature);
- }
+    // @Output() featureSelected = new EventEmitter<string>();
+        // onSelect(feature: string) {
+        //     this.featureSelected.emit(feature);
+//  }
 }
