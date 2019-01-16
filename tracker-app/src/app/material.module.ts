71c3f933a693c5ab1235629082bfb8ed8942861a
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-01-15 16:57:50 -0800

    checbox position

--- tracker-app/src/app/material.module.ts
+++ tracker-app/src/app/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
+         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule ({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
 
 export class MaterialModule {}
