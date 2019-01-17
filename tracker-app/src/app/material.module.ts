b7eedfe0d22052492ff768384df588bf557a524d
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-01-16 17:02:01 -0800

    adding a navigation

--- tracker-app/src/app/material.module.ts
+++ tracker-app/src/app/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
-         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material';
+         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
+         MatSidenavModule } from '@angular/material';
 
 @NgModule ({
     imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
-             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule],
     exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
-             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule]
 })
 
 export class MaterialModule {}
