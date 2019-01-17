9e4cfa9e12cfa0b708f7c218b770d07446af9615
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-01-16 17:10:52 -0800

    toolbar setup

--- tracker-app/src/app/material.module.ts
+++ tracker-app/src/app/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
          MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-         MatSidenavModule } from '@angular/material';
+         MatSidenavModule, MatToolbarModule } from '@angular/material';
 
 @NgModule ({
     imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
-             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule],
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
+             MatToolbarModule],
     exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
-             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule]
+             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
+             MatToolbarModule]
 })
 
 export class MaterialModule {}
