b4a0881e86790b9755323ac193474165adc0a94c
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-05 15:45:10 -0800

    progress spinner implementation

--- tracker-app/src/app/material.module.ts
+++ tracker-app/src/app/material.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
          MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
          MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule,
-         MatCardModule, MatSelectModule } from '@angular/material';
+         MatCardModule, MatSelectModule, MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule ({
     imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
              MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule],
+             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+             MatProgressSpinnerModule],
     exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
              MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule]
+             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+             MatProgressSpinnerModule]
 })
 
 export class MaterialModule {}
