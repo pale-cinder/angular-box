451760c120e066a2bf53cae7365818c2bbd364e7
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-04 23:00:11 -0800

    card dropdown options

--- tracker-app/src/app/material.module.ts
+++ tracker-app/src/app/material.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
          MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
          MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule,
-         MatCardModule } from '@angular/material';
+         MatCardModule, MatSelectModule } from '@angular/material';
 
 @NgModule ({
     imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
              MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule],
+             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule],
     exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
              MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule]
+             MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule]
 })
 
 export class MaterialModule {}
