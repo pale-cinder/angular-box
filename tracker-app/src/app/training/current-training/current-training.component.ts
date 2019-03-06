8a5ff8e7b52dc4ce15f0a7ef811860fd47e1bebb
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-05 16:20:12 -0800

    implemented spinner grow every second

--- tracker-app/src/app/training/current-training/current-training.component.ts
+++ tracker-app/src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
 
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+      // every second it's increasing by 5
+      this.progress = this.progress + 5;
+    }, 1000)
   }
 
 }
