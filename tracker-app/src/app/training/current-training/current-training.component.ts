f01235c0732cc653a155a1f14522d3db64d3189b
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-07 17:34:34 -0800

    resume and cintinue

--- tracker-app/src/app/training/current-training/current-training.component.ts
+++ tracker-app/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from './stop-training.components';
 
@@ -8,12 +8,18 @@ import { StopTrainingComponent } from './stop-training.components';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
+  
   progress = 0;
   timer: any;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  } 
+
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       // every second it's increasing by 5
       this.progress = this.progress + 5;
@@ -28,11 +34,16 @@ export class CurrentTrainingComponent implements OnInit {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {data: {
       progress: this.progress
-    }});
+    }
+  });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
-    })
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
   }
 
 }
