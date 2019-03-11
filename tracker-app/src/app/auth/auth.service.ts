1fa5738c679fea949dfd5ad881e3b5860b2bed16
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-10 17:45:28 -0700

    implementing an authentication

---
+++ tracker-app/src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+
+export class AuthService {
+    //to fake a user login and inform the other parts about the user login
+    private user: User;
+
+    registerUser(authData: AuthData) {
+        this.user = {
+            email: authData.email,
+            userId: Math.round(Math.random() * 10000).toString()
+        };
+    }
+
+    login(authData: AuthData) {
+        this.user = {
+            email: authData.email,
+            userId: Math.round(Math.random() * 10000).toString()
+        };
+    }
+
+    logout() {
+        this.user = null;
+    }
+
+    getUser() {
+        return { ...this.user };
+    }
+}
