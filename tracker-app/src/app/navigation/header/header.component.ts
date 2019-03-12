65c85aaa61db7d77b86f9c303cff25ec32a65403
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-03-11 23:19:46 -0700

    authentification for login

--- tracker-app/src/app/navigation/header/header.component.ts
+++ tracker-app/src/app/navigation/header/header.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthData } from 'src/app/auth/auth-data.model';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   //make this listenable from outside for the event whenever we click the menu button
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }
